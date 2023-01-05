dce8987ba100149296b96f59cd9374ce8520c393
Devyn Guerra <113082044+dachin0@users.noreply.github.com>
2023-01-04 18:06:01 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,192 +1,123 @@
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
-
-/*the size of the canvas*/
-canvas.width = 1280
-canvas.height = 720
-
-c.fillRect(0, 0, canvas.width, canvas.height)
-
-/*each characters fall down at 0.8*/
-const falling = 0.8
-
-/*created the player*/
-class teHunt {
-    constructor({position, velocity, color = 'purple'}) {
-        this.position = position
-        this.velocity= velocity
-        this.width = 50
-        this.height = 150
-        this.lastMovement
-        this.attackMove = {
-            position: this.position,
-            width: 100,
-            height: 50
-        }
-        this.color = color
-        this.hitBox
+let gameState = 'start';
+let paddle_1 = document.querySelector('.paddle_1');
+let paddle_2 = document.querySelector('.paddle_2');
+let board = document.querySelector('.board');
+let initial_ball = document.querySelector('.ball');
+let ball = document.querySelector('.ball');
+let score_1 = document.querySelector('.player_1_score');
+let score_2 = document.querySelector('.player_2_score');
+let message = document.querySelector('.message');
+let paddle_1_coord = paddle_1.getBoundingClientRect();
+let paddle_2_coord = paddle_2.getBoundingClientRect();
+let initial_ball_coord = ball.getBoundingClientRect();
+let ball_coord = initial_ball_coord;
+let board_coord = board.getBoundingClientRect();
+let paddle_common = 
+    document.querySelector('.paddle').getBoundingClientRect();
+  
+let dx = Math.floor(Math.random() * 4) + 3;
+let dy = Math.floor(Math.random() * 4) + 3;
+let dxd = Math.floor(Math.random() * 2);
+let dyd = Math.floor(Math.random() * 2);
+  
+document.addEventListener('keydown', (e) => {
+  if (e.key == 'Enter') {
+    gameState = gameState == 'start' ? 'play' : 'start';
+    if (gameState == 'play') {
+      message.innerHTML = 'Game Started';
+      message.style.left = 42 + 'vw';
+      requestAnimationFrame(() => {
+        dx = Math.floor(Math.random() * 4) + 3;
+        dy = Math.floor(Math.random() * 4) + 3;
+        dxd = Math.floor(Math.random() * 2);
+        dyd = Math.floor(Math.random() * 2);
+        moveBall(dx, dy, dxd, dyd);
+      });
     }
-    terrorist(){
-        /*characters*/
-        c.fillStyle = this.color
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-
-        /*attack*/
-        c.fillStyle = 'red'
-        c.fillRect(this.attackMove.position.x, this.attackMove.position.y, this.attackMove.width, this.attackMove.height)
+  }
+  if (gameState == 'play') {
+    if (e.key == 'w') {
+      paddle_1.style.top =
+        Math.max(
+          board_coord.top,
+          paddle_1_coord.top - window.innerHeight * 0.06
+        ) + 'px';
+      paddle_1_coord = paddle_1.getBoundingClientRect();
     }
-
-    update() {
-        this.terrorist()
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-        /*making the characters stay above the canvas*/
-        if (this.position.y + this.height + this.velocity.y >= canvas.height){
-            this.velocity.y = 0
-        }else this.velocity.y += falling
+    if (e.key == 's') {
+      paddle_1.style.top =
+        Math.min(
+          board_coord.bottom - paddle_common.height,
+          paddle_1_coord.top + window.innerHeight * 0.06
+        ) + 'px';
+      paddle_1_coord = paddle_1.getBoundingClientRect();
     }
-    dmg(){
-        this.hitBox = true
-        setTimeout(() => {
-            this.hitBox = false
-        }, 100)
+  
+    if (e.key == 'ArrowUp') {
+      paddle_2.style.top =
+        Math.max(
+          board_coord.top,
+          paddle_2_coord.top - window.innerHeight * 0.1
+        ) + 'px';
+      paddle_2_coord = paddle_2.getBoundingClientRect();
     }
+    if (e.key == 'ArrowDown') {
+      paddle_2.style.top =
+        Math.min(
+          board_coord.bottom - paddle_common.height,
+          paddle_2_coord.top + window.innerHeight * 0.1
+        ) + 'px';
+      paddle_2_coord = paddle_2.getBoundingClientRect();
+    }
+  }
+});
+  
+function moveBall(dx, dy, dxd, dyd) {
+  if (ball_coord.top <= board_coord.top) {
+    dyd = 1;
+  }
+  if (ball_coord.bottom >= board_coord.bottom) {
+    dyd = 0;
+  }
+  if (
+    ball_coord.left <= paddle_1_coord.right &&
+    ball_coord.top >= paddle_1_coord.top &&
+    ball_coord.bottom <= paddle_1_coord.bottom
+  ) {
+    dxd = 1;
+    dx = Math.floor(Math.random() * 4) + 3;
+    dy = Math.floor(Math.random() * 4) + 3;
+  }
+  if (
+    ball_coord.right >= paddle_2_coord.left &&
+    ball_coord.top >= paddle_2_coord.top &&
+    ball_coord.bottom <= paddle_2_coord.bottom
+  ) {
+    dxd = 0;
+    dx = Math.floor(Math.random() * 4) + 3;
+    dy = Math.floor(Math.random() * 4) + 3;
+  }
+  if (
+    ball_coord.left <= board_coord.left ||
+    ball_coord.right >= board_coord.right
+  ) {
+    if (ball_coord.left <= board_coord.left) {
+      score_2.innerHTML = +score_2.innerHTML + 1;
+    } else {
+      score_1.innerHTML = +score_1.innerHTML + 1;
+    }
+    gameState = 'start';
+  
+    ball_coord = initial_ball_coord;
+    ball.style = initial_ball.style;
+    message.innerHTML = 'Press Enter to Play Pong';
+    message.style.left = 38 + 'vw';
+    return;
+  }
+  ball.style.top = ball_coord.top + dy * (dyd == 0 ? -1 : 1) + 'px';
+  ball.style.left = ball_coord.left + dx * (dxd == 0 ? -1 : 1) + 'px';
+  ball_coord = ball.getBoundingClientRect();
+  requestAnimationFrame(() => {
+    moveBall(dx, dy, dxd, dyd);
+  });
 }
-
-/*created the first player's position*/
-const twitch = new teHunt({
-    position: {
-    x:0,
-    y:580
-},
-    velocity: {
-        x:0,
-        y:0
-    }
-})
-
-/*created the second player position*/
-const jager = new teHunt({
-    position: {
-    x:1230,
-    y:580
-},
-    velocity: {
-        x:0,
-        y:0
-    },
-    color: 'pink'
-})
-
-/*doesn't cancel's a character's movement when lifting a key*/
-const doesntCancelMovement = {
-    a: {
-        pressed: false
-    },
-    d: {
-        pressed: false
-    },
-    ArrowRight: {
-        pressed:false
-    },
-    ArrowLeft: {
-        pressed:false
-    }
-}
-
-
-
-/*Constantly updating the animation*/
-function movement() {
-    window.requestAnimationFrame(movement)
-    c.fillStyle = 'black'
-    c.fillRect(0, 0, canvas.width, canvas.height)
-    twitch.update()
-    jager.update()
-
-    /*player one movement*/
-    twitch.velocity.x = 0
-    if (doesntCancelMovement.a.pressed && twitch.lastMovement === 'a') {
-        twitch.velocity.x = -3
-    } else if (doesntCancelMovement.d.pressed && twitch.lastMovement === 'd') {
-        twitch.velocity.x = 3
-    }
-
-    /*player two movement*/
-    jager.velocity.x = 0
-    if (doesntCancelMovement.ArrowLeft.pressed && jager.lastMovement === 'ArrowLeft') {
-        jager.velocity.x = -3
-    } else if (doesntCancelMovement.ArrowRight.pressed && jager.lastMovement === 'ArrowRight') {
-        jager.velocity.x = 3
-    }
-
-    /*attacking*/
-    if (twitch.attackMove.position.x + twitch.attackMove.width >= jager.position.x &&
-         twitch.attackMove.position.x <= jager.position.x + jager.width &&
-          twitch.attackMove.position.y + twitch.attackMove.height >= jager.position.y &&
-          twitch.attackMove.position.y <= jager.position.y + jager.height &&
-          player.hitBox) {
-            player.hitBox = false
-    }
-}
-
-/*when pressing down on an key, the character moves at a velocity of 1 px*/
-window.addEventListener('keydown', (event) => {
-    switch (event.key) {
-        /*player one*/
-        case 'd':
-            doesntCancelMovement.d.pressed = true
-            twitch.lastMovement = 'd'
-            break
-        case 'a':
-            doesntCancelMovement.a.pressed = true
-            twitch.lastMovement = 'a'
-            break
-        case 'w':
-            twitch.velocity.y = -15
-            break
-        case 'e':
-            player.hitBox()
-            break
-        /*player two*/
-        case 'ArrowRight':
-            doesntCancelMovement.ArrowRight.pressed = true
-            jager.lastMovement = 'ArrowRight'
-            break
-        case 'ArrowLeft':
-            doesntCancelMovement.ArrowLeft.pressed = true
-            jager.lastMovement = 'ArrowLeft'
-            break
-        case 'ArrowUp':
-            jager.velocity.y = -15
-            break
-    }
-    console.log(event.key)
-})
-
-/*when lefting up from the key, the character stops moving*/
-window.addEventListener('keyup', (event) => {
-    /*first player's key*/
-    switch (event.key) {
-        case 'd':
-            doesntCancelMovement.d.pressed = false
-            break
-        case 'a':
-            doesntCancelMovement.a.pressed = false
-            break
-    }
-    /*second player's key*/
-    switch (event.key) {
-        case 'ArrowRight':
-            doesntCancelMovement.ArrowRight.pressed = false
-            break
-        case 'ArrowLeft':
-            doesntCancelMovement.ArrowLeft.pressed = false
-            break
-    }
-
-    console.log(event.key)
-})
-
-console.log(twitch)
-movement()
